a7e92c5587d0b26b79484492b1c336badf211bcd
Hakob <hakobv68@gmail.com>
2022-12-27 08:41:51 -0800

    Method Override

--- src/crew/Hakob.java
+++ src/crew/Hakob.java
@@ -1,12 +1,36 @@
 package crew;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Hakob extends StudentInfo {
 
     @Override
     public String method() {
-        return null;
+
+
+
+        System.err.println("Enter the date when you were applied in the following format Month/Day/Year->four digits:");
+
+        String jobApplyDate = new Scanner(System.in).next();
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("M/d/y");
+
+        LocalDate jobApD = LocalDate.parse(jobApplyDate,df);
+
+        int experience = LocalDate.now().getYear() - jobApD.getYear();
+        if(experience > 1){
+            return String.valueOf(experience) + " years";
+        }else {
+            return String.valueOf(experience) + " year";
+        }
+
     }
+
+
 }
+
 /*
 1.Hakob
 Your method should request from user to fill up :
